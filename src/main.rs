c73088f6394b0dc02269a729856ff9f8b5bdca33
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2024-12-28 01:47:26 -0800

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use std::path::Iter;
+
 use macroquad::{prelude::*, time};
 
-const SAND_SIZE : f32 = 10.0;
-const PHYSICS_TICK_MILLIS : f32 = 0.02;
+const SAND_SIZE : f32 = 4.0;
+const PHYSICS_TICK_MILLIS : f32 = 0.008;
 
 #[macroquad::main("Main")]
 async fn main() {    
     let mut blocks : Vec<Vec<u32>> = Vec::new();
 
     let sand_colors = vec![
-        // Color::from_hex(0xffe0ab),
-        // Color::from_hex(0xf3ce93), 
-        Color::from_hex(0xedc9af), 
+        Color::from_hex(0xffe0ab),
+        Color::from_hex(0xf3ce93), 
+        Color::from_hex(0xd6b588), 
         Color::from_hex(0xffe0ab), 
         Color::from_hex(0xdab984), 
     ];
@@ -44,42 +46,63 @@ async fn main() {
             blocks = (0..b_width).map(|x| (0..b_height).map(|y| { 0 }).collect()).collect();
         }
 
-        // draw additional pixels
-        if is_mouse_button_down(MouseButton::Left) {
-            // draw_rectangle(mouse_position().0, mouse_position().1, 10.0, 10.0, WHITE);
-            let (x, y) = ((x / SAND_SIZE).floor() as usize, (y / SAND_SIZE).floor() as usize);
-
-            for i in -1..1 {
-                for j in -1..1 {
-                    let X = ((x as i32) + i) as usize;
-                    let Y = ((y as i32) + j) as usize;
-
-                    match blocks.get(X) {
-                        Some(s) => {
-                            match s.get(Y) {
-                                // Some(q) => if *q == 0 {blocks[X][Y] = rand::gen_range(1, 6)},
-                                Some(q) => if *q == 0 {blocks[X][Y] = 1},
-                                None => (),
-                            }
-                        }
-                        None => ()
-                    }
-                    // blocks[x][y] = rand::gen_range(1, 6); 
-                }
-            }
-        }
-
-
 
         if acc_time > PHYSICS_TICK_MILLIS {
+            
+            // draw additional pixels
+            if is_mouse_button_down(MouseButton::Left) {
+                // draw_rectangle(mouse_position().0, mouse_position().1, 10.0, 10.0, WHITE);
+                let (x, y) = ((x / SAND_SIZE).floor() as usize, (y / SAND_SIZE).floor() as usize);
+
+                for i in -2..2 {
+                    for j in -2..2 {
+                        let X = ((x as i32) + i) as usize;
+                        let Y = ((y as i32) + j) as usize;
+
+                        match blocks.get(X) {   
+                            Some(s) => {
+                                match s.get(Y) {
+                                    Some(q) => if *q == 0 && rand::gen_range(0, 2) != 1 {blocks[X][Y] = rand::gen_range(1, 6)},
+                                    // Some(q) => if *q == 0 {blocks[X][Y] = 1},
+                                    None => (),
+                                }
+                            }
+                            None => ()
+                        }
+                        // blocks[x][y] = rand::gen_range(1, 6); 
+                    }
+                }
+            }
+
+            let row_iter: Vec<u32> = if rand::gen_range(0, 2) == 1 {
+                (0..b_width-1).collect()
+            } else {
+                (0..b_width-1).rev().collect()
+            };
+
             // game of life
-            for x in 0..b_width {
-                for y in (0..b_height).rev() {
-                    let x = x as usize;
+            for y in (0..b_height).rev() {
+                for x in row_iter.iter(){
+                    let x = *x as usize;
                     let y = y as usize;
-                    if blocks[x][y] != 0 && y != (b_height as usize)-1 && blocks[x][y+1] == 0 {
-                        blocks[x][y+1] = blocks[x][y];
-                        blocks[x][y] = 0;
+                    if blocks[x][y] != 0 && y != (b_height as usize)-1 {
+                        let left = x != 0 && blocks[x-1][y+1] == 0;
+                        let right = x != (b_width as usize) - 1 && blocks[x+1][y+1] == 0;
+
+                        if blocks[x][y+1] == 0 {
+                            blocks[x][y+1] = blocks[x][y];
+                            blocks[x][y] = 0;
+                        } else if left && right{
+                            let offset: i32 = if rand::gen_range(0, 2) == 1 {1} else {-1};
+                            blocks[x][((y as i32) + offset) as usize] = blocks[x][y];
+                            blocks[x][y] = 0;
+                        } else if left {
+                            blocks[x-1][y+1] = blocks[x][y];
+                            blocks[x][y] = 0;
+                        } else if right {
+                            blocks[x+1][y+1] = blocks[x][y];
+                            blocks[x][y] = 0;
+                        }
                     }
                 }
             }
